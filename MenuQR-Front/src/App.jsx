2bba230c131cb083656b3f57581f35aa5dbf50c1
NourElImane <mn_elbar@esi.dz>
2025-07-31 11:36:45 +0100

    1st commit

---
+++ MenuQR-Front/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <p className="text-3xl font-bold underline bg-amber-800">HELLO MENU QR</p>
+    </>
+  )
+}
+
+export default App
